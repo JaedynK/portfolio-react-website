f3c8eb8441061889837493e64b99245a8fc6e65c
Jaedyn Keister <jaedyn.keister@gmail.com>
2023-04-25 17:12:11 -0500

    slow and steady

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,18 @@ import AboutPage from './components/About'
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const video = {
+    src: 'path/to/video.mp4',
+    width: '100%',
+    height: '100%',
+    loop: true,
+    autoplay: true,
+    muted: true,
+    controls: false,
+    playsinline: true,
+  };
 
   return (
     <div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,58 +5,109 @@ import Col from 'react-bootstrap/Col';
 import Figure from 'react-bootstrap/Figure';
 import Typewriter from 'typewriter-effect';
 
-export default function HomePage(){
+export default function HomePage() {
 
-    return(
-        <div>
-            <h1  className='basicText' >Welcome to my website! </h1>
-    <Container>
-      <Row md={2} >
-        <Col>
-        <Figure>
-      <Figure.Image
-        width={500}
-        height={500}
-        alt="500x500"
-        src= '/public/images/me.JPG'
-      />
-      </Figure>
-        </Col>
-        <Col lg={true} className='basicText'>
-        <Typewriter
-  onInit={(typewriter) => {
-    typewriter.typeString(`
-    As a Full Stack Software Engineer and Military Veteran 
-    with 2+ years of management experience in the US Air Force,
-    I bring a unique set of skills and expertise to any project.
-    `)
-      .callFunction(() => {
-        console.log('String typed out!');
-      })
-      .pauseFor(1000)
-      .callFunction(() => {
-        console.log('All strings were deleted');
-      })
-      .start();
-    //   .deleteAll()
-  }}
-/>
-        </Col>
+
+
+  return (
+    <div>
+      <h1 >Let me bring <Typewriter
+        options={{
+          strings: ['perfection', 'speed', 'gusto', 'quality', "value"],
+          autoStart: true,
+          loop: true,
+        }}
+      /> to your project!! </h1>
+      <Container>
+        <Row  >
+          <Col>
+            <div className='image-container'>
+              <Figure>
+                <Figure.Image
+                  width="500"
+                  height="500"
+                  src='/images/me.JPG'
+                />
+              </Figure>
+            </div>
+          </Col>
+          <Col >
+            <div className='basicText'>
+              As a Full Stack Software Engineer and United State Air Force Veteran
+              I bring a unique set of skills and expertise to any project.
+
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <div className='skillContainer'>
+      <Container>
+      <Row>
+        <Col md={{ span: 4, offset: 4 }}><Figure.Image
+                  width="100"
+                  height="100"
+                  src='/images/react.svg'
+                  /></Col>
       </Row>
       <Row>
-        <Col className='basicText'>
-        <Typewriter
-        options={{
-            strings: ['Hello', 'World'],
-            autoStart: true,
-            loop: false,
-        }}
-        />
+        <Col md={{ span: 4, offset: 1}}><Figure.Image
+                  width="100"
+                  height="100"
+                  src='/images/git.svg'
+                  /></Col>
+            <Col md={{ span: 4, offset: 2 }}><Figure.Image
+                  width="100"
+                  height="100"
+                  src='/images/github.svg'
+                  /></Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 4 }}>
+          <Figure.Image
+                  width="100"
+                  height="100"
+                  src='/images/html5.svg'
+                  />
         </Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
+        <Col md={{ span: 4, offset: 4 }}><Figure.Image
+                  width="100"
+                  height="100"
+                  src='/images/css3-alt.svg'
+                  /></Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 3, offset: 2 }}><Figure.Image
+                  width="100"
+                  height="100"
+                  src='/images/square-js.svg'
+                /></Col>
+        <Col md={{ span: 3, offset: 2 }}><Figure.Image
+                  width="100"
+                  height="100"
+                  src='/images/python.svg'
+                /></Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}><Figure.Image
+                  width="100"
+                  height="100"
+                  src='/images/docker.svg'
+                  /></Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 3, }}><Figure.Image
+                  width="300"
+                  height="300"
+                  src='/images/fastapi.png'
+                /></Col>
+        <Col md={{ span: 3, offset: 6 }}><Figure.Image
+                  width="300"
+                  height="200"
+                  src='/images/django.png'
+                /></Col>
       </Row>
     </Container>
-        </div>
-    )
+    </div>
+    </div>
+  )
 }
